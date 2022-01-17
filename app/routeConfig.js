428473a9b8c79199092a99b6f8c49ab73f456ad1
Naeem Dadi <naeem@wednesday.is>
2022-01-17 10:47:41 +0530

    Route config cleanup

--- app/routeConfig.js
+++ app/routeConfig.js
@@ -3,10 +3,6 @@ import routeConstants from '@utils/routeConstants';
 // import HomeContainer from './containers/HomeContainer/Loadable';
 import ItunesContainer from './containers/ItunesContainer/Loadable';
 export const routeConfig = {
-  // repos: {
-  //   component: HomeContainer,
-  //   ...routeConstants.repos
-  // },
   itunes: {
     component: ItunesContainer,
     ...routeConstants.repos
--- app/utils/routeConstants.js
+++ app/utils/routeConstants.js
@@ -1,12 +1,4 @@
 export default {
-  // repos: {
-  //   route: '/repos',
-  //   props: {
-  //     maxwidth: 500,
-  //     padding: 20
-  //   },
-  //   exact: true
-  // },
   repos: {
     route: '/',
     props: {
